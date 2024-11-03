f62c2089420be2c86308ed4481851f0baee1831d
rdayltx <rdayltx@gmail.com>
2024-11-03 19:21:11 -0300

    fix incorrect exclude ml/s to amz/s

--- assets/userscripts/ML_Amz_Clear_URL_Parameters.js
+++ assets/userscripts/ML_Amz_Clear_URL_Parameters.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          ML, Amz. Clear URL Parameters
 // @namespace     Clear URL Parameters
-// @version       2.6
+// @version       2.7
 // @icon          https://raw.githubusercontent.com/rdayltx/daylightdev.github.io/refs/heads/master/assets/userscripts/clear_url.ico
 // @description   Remove parâmetros desnecessários de URLs de sites especificos
 // @author        DayLight
@@ -24,7 +24,6 @@
 // @exclude-match https://www.mercadolivre.com.br/my-reviews*
 // @exclude-match https://www.mercadolivre.com.br/perguntas/*
 // @exclude-match https://www.mercadolivre.com.br/navigation*
-// @exclude-match https://www.mercadolivre.com.br/s*
 //
 // ============== MAGAZINE LUIZA ==============
 // @match https://www.magazinevoce.com.br/*
@@ -41,7 +40,7 @@
 // @exclude-match https://www.amazon.com.br/myk*
 // @exclude-match https://www.amazon.com.br/b/*
 // @exclude-match https://www.amazon.com.br/gp/*
-
+// @exclude-match https://www.amazon.com.br/s*
 // @exclude-match https://www.amazon.com.br/prime*
 // @exclude-match https://www.amazon.com.br/gcx*
 // @exclude-match https://www.amazon.com.br/baby-reg*
